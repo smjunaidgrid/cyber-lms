674412cac053293ce6467a369824d133f9968746
smjunaidgrid <moshaik@griddynamics.com>
2026-02-16 10:31:31 +0530

    Config management
    Environment variables
    
    Database connection layer
    
    Proper app structure
    
    Scalable architecture foundation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.database import engine, Base
 
-app = FastAPI(title="Interactive Cybersecurity Awareness Training Platform")
+app = FastAPI(title="Cybersecurity Awareness Platform")
+
+# Create DB tables automatically (temporary for dev)
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
-    return {"message" : "Cyber Training API building"}
+    return {"message": "Cyber Training API Running....."}
