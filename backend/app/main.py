aaaa34a46af71d182ad8bb18e07b510e781905ca
smjunaidgrid <moshaik@griddynamics.com>
2026-02-17 23:53:01 +0530

    backend engineerning Secure password hashing
    JWT authentication
    
    Role storage
    
    Proper DB session handling
    
    Structured backend layering

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from app.database import engine, Base
+from app.models import user
+from app.routers import auth
 
 app = FastAPI(title="Cybersecurity Awareness Platform")
 
-# Create DB tables automatically (temporary for dev)
 Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
-    return {"message": "Cyber Training API Running....."}
+    return {"message": "Cyber Training API Running"}
---
+++ backend/app/schemas/user.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    role: str
+
+    class Config:
+        orm_mode = True
